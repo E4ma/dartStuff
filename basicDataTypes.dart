12e4bf13f85e601a2dbd6dcf320c7c4c252fccd1
e4ma <ifeomaidoko@.com>
2023-11-06 20:30:09 -0700

    basic data types

---
+++ basicDataTypes.dart
@@ -0,0 +1,30 @@
+void main() {
+  // Data Types - all objs
+  // Numbers - int & double only
+  // int age = 10; // type, var name & value
+  
+  // Numbers: int
+  int score = 23;
+  const count = 23; // inferred as integer automatically by compiler
+  
+  int hexValue = 0xEADEBAEE;
+  
+  // Numbers: double
+  double percentage = 93.4;
+  const percent = 93.45;
+  double exponents = 1.425e5;
+  
+  // Strings
+  String name = 'Henry';
+  const company = 'Google';
+  
+  
+  // Boolean
+  bool isValid = true;
+  const isAlive = false;
+  
+  print(score);
+  print (isValid);
+  print(hexValue);
+  print(exponents);
+}
