31aba146c3b3f08d5cb42097c7489a1155e6a03c
e4ma <ifeomaidoko@.com>
2023-11-06 20:15:15 -0700

    comments

--- main.dart
+++ main.dart
@@ -1,6 +1,11 @@
 void main() {
-  print('Hello World');
+  // This is my first line of code
+  print('Hello World'); // this is another comment
   print('This is my first application');
+
+  // Performing an arithmetic operation
   print(4 - 1);
+
+  // printing a boolean value
   print(true);
 }
